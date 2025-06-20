70403170998a5e025993e862630c84794a8d7255
Jo.Smith40 <jo.smith40@yahoo.com>
2025-06-20 19:17:48 +0200

    V1.0.9 Automatisch: Symbole in einem Grid

--- main/main.c
+++ main/main.c
@@ -4,266 +4,187 @@
 #include <stdio.h>
 #include <esp_log.h>
 #include <esp_timer.h>
-#include <math.h>
-#include <esp_task_wdt.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "t_display_s3.h"
-//#include "iot_button.h"
-//#include "button_gpio.h"
+#include "lvgl.h"
 
+static int symbol_draw_index = 0;
 
 #define TAG "main"
 
-/*
-// used for setup_test_ui() function
-char *power_icon = LV_SYMBOL_POWER;
-int battery_voltage;
-int battery_percentage;
-bool on_usb_power = false;
-int current_battery_symbol_idx = 0;
-char *battery_symbols[5] = {
-        LV_SYMBOL_BATTERY_EMPTY,
-        LV_SYMBOL_BATTERY_1,
-        LV_SYMBOL_BATTERY_2,
-        LV_SYMBOL_BATTERY_3,
-        LV_SYMBOL_BATTERY_FULL
-};
-
-char *wlan_icon = LV_SYMBOL_WIFI;
-char *bluetooth_icon = LV_SYMBOL_BLUETOOTH;
-char *gps_icon = LV_SYMBOL_GPS;
-char *audio_icon = LV_SYMBOL_AUDIO;
-char *video_icon = LV_SYMBOL_VIDEO;
-char *list_icon = LV_SYMBOL_LIST;
-
-// lvgl ui elements
-lv_obj_t *side_bar;
 lv_obj_t *top_bar;
-lv_obj_t *bottom_bar;
-lv_obj_t *lbl_power_mode;
-lv_obj_t *lbl_battery_pct;
-lv_obj_t *lbl_voltage;
-lv_obj_t *lbl_power_icon;
-lv_obj_t *screen_brightness;
-
-lv_obj_t *lbl_wlan_icon;
-lv_obj_t *lbl_bluetooth_icon;
-lv_obj_t *lbl_gps_icon;
-lv_obj_t *lbl_audio_icon;
-lv_obj_t *lbl_video_icon;
-lv_obj_t *lbl_list_icon;
-*/
-lv_obj_t *bottom_bar;
-
-
 
 TaskHandle_t lcd_brightness_task_hdl;
 esp_timer_handle_t lcd_brightness_timer_hdl;
 
+extern lv_obj_t *top_bar;
 
+extern const char *symbols[] = {
+    LV_SYMBOL_BULLET,
+    LV_SYMBOL_AUDIO,
+    LV_SYMBOL_VIDEO,
+    LV_SYMBOL_LIST,
+    LV_SYMBOL_OK,
+    LV_SYMBOL_CLOSE,
+    LV_SYMBOL_POWER,
+    LV_SYMBOL_SETTINGS,
+    LV_SYMBOL_HOME,
+    LV_SYMBOL_DOWNLOAD,
+    LV_SYMBOL_DRIVE,
+    LV_SYMBOL_REFRESH,
+    LV_SYMBOL_MUTE,
+    LV_SYMBOL_VOLUME_MID,
+    LV_SYMBOL_VOLUME_MAX,
+    LV_SYMBOL_IMAGE,
+    LV_SYMBOL_TINT,
+    LV_SYMBOL_PREV,
+    LV_SYMBOL_PLAY,
+    LV_SYMBOL_PAUSE,
+    LV_SYMBOL_STOP,
+    LV_SYMBOL_NEXT,
+    LV_SYMBOL_EJECT,
+    LV_SYMBOL_LEFT,
+    LV_SYMBOL_RIGHT,
+    LV_SYMBOL_PLUS,
+    LV_SYMBOL_MINUS,
+    LV_SYMBOL_EYE_OPEN,
+    LV_SYMBOL_EYE_CLOSE,
+    LV_SYMBOL_WARNING,
+    LV_SYMBOL_SHUFFLE,
+    LV_SYMBOL_UP,
+    LV_SYMBOL_DOWN,
+    LV_SYMBOL_LOOP,
+    LV_SYMBOL_DIRECTORY,
+    LV_SYMBOL_UPLOAD,
+    LV_SYMBOL_CALL,
+    LV_SYMBOL_CUT,
+    LV_SYMBOL_COPY,
+    LV_SYMBOL_SAVE,
+    LV_SYMBOL_BARS,
+    LV_SYMBOL_ENVELOPE,
+    LV_SYMBOL_CHARGE,
+    LV_SYMBOL_PASTE,
+    LV_SYMBOL_BELL,
+    LV_SYMBOL_KEYBOARD,
+    LV_SYMBOL_GPS,
+    LV_SYMBOL_FILE,
+    LV_SYMBOL_WIFI,
+    LV_SYMBOL_BATTERY_FULL,
+    LV_SYMBOL_BATTERY_3,
+    LV_SYMBOL_BATTERY_2,
+    LV_SYMBOL_BATTERY_1,
+    LV_SYMBOL_BATTERY_EMPTY,
+    LV_SYMBOL_USB,
+    LV_SYMBOL_BLUETOOTH,
+    LV_SYMBOL_TRASH,
+    LV_SYMBOL_EDIT,
+    LV_SYMBOL_BACKSPACE,
+    LV_SYMBOL_SD_CARD,
+    LV_SYMBOL_NEW_LINE,
+    LV_SYMBOL_DUMMY,
+};
 
-void ui_init() {
-
-    bottom_bar = lv_obj_create(lv_screen_active());
-    lv_obj_align(bottom_bar, LV_ALIGN_BOTTOM_RIGHT, 0, 0);
-    //lv_obj_set_width(bottom_bar, LCD_H_RES - 50);
-    //lv_obj_set_height(bottom_bar, 130);
-    lv_obj_set_width(bottom_bar, LCD_H_RES);  //* set width to full screen width
-    lv_obj_set_height(bottom_bar, LCD_V_RES); // set height to full screen height
-    lv_obj_remove_flag(bottom_bar, LV_OBJ_FLAG_SCROLLABLE);
-    lv_obj_set_style_radius(bottom_bar, 0, LV_PART_MAIN | LV_STATE_DEFAULT);
-    lv_obj_set_style_border_width(bottom_bar, 0, LV_PART_MAIN | LV_STATE_DEFAULT);
-    lv_obj_set_style_bg_opa(bottom_bar, 255, LV_PART_MAIN | LV_STATE_DEFAULT);                      // Full opacity
-    lv_obj_set_style_bg_color(bottom_bar, lv_color_hex(0xADD8E6), LV_PART_MAIN | LV_STATE_DEFAULT);
-
-
-    /*
-        lbl_wlan_icon = lv_label_create(bottom_bar);
-    lv_obj_align(lbl_wlan_icon, LV_ALIGN_TOP_RIGHT, 0, 0);
-
-    lbl_bluetooth_icon = lv_label_create(bottom_bar);
-    lv_obj_align(lbl_bluetooth_icon, LV_ALIGN_TOP_RIGHT, -25, 0);
-
-    lbl_gps_icon = lv_label_create(bottom_bar);
-    lv_obj_align(lbl_gps_icon, LV_ALIGN_TOP_RIGHT, -50, 0);
-
-    lbl_audio_icon = lv_label_create(bottom_bar);
-    lv_obj_align(lbl_audio_icon, LV_ALIGN_TOP_RIGHT, -75, 0);
-
-    lbl_video_icon = lv_label_create(bottom_bar);
-    lv_obj_align(lbl_video_icon, LV_ALIGN_TOP_RIGHT, -100, 0);
-
-    lbl_list_icon = lv_label_create(bottom_bar);
-    lv_obj_align(lbl_list_icon, LV_ALIGN_TOP_RIGHT, -125, 0);
-
-    */
-
-
-
+void ui_init()
+{
+    top_bar = lv_obj_create(lv_screen_active());
+    lv_obj_align(top_bar, LV_ALIGN_BOTTOM_RIGHT, 0, 0);
+    // lv_obj_set_width(top_bar, LCD_H_RES);
+    // lv_obj_set_height(top_bar, 130);
+    lv_obj_set_width(top_bar, LCD_H_RES);  //* set width to full screen width
+    lv_obj_set_height(top_bar, LCD_V_RES); // set height to full screen height
+    lv_obj_remove_flag(top_bar, LV_OBJ_FLAG_SCROLLABLE);
+    lv_obj_set_style_radius(top_bar, 0, LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_obj_set_style_border_width(top_bar, 0, LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_obj_set_style_bg_opa(top_bar, 255, LV_PART_MAIN | LV_STATE_DEFAULT); // Full opacity
+    lv_obj_set_style_bg_color(top_bar, lv_color_hex(0xADD8E6), LV_PART_MAIN | LV_STATE_DEFAULT);
 }
 
-/*
-static void update_hw_info_timer_cb(void *arg) {
-    battery_voltage = get_battery_voltage();
-    on_usb_power = usb_power_voltage(battery_voltage);
-    battery_percentage = (int) volts_to_percentage((double) battery_voltage / 1000);
-}
-*/
 
+#define SYMBOLS_PER_ROW 15
+#define SPACING_X 20
+#define SPACING_Y 20
 
+void create_symbol_grid()
+{
+    lv_obj_clean(top_bar); // Alte Inhalte löschen (optional)
 
+    // Grid-Spalten definieren (15 Spalten mit fester Breite + spacing)
+    static lv_coord_t col_dsc[SYMBOLS_PER_ROW + 1];
+    for (int i = 0; i < SYMBOLS_PER_ROW; ++i)
+        col_dsc[i] = LV_GRID_FR(1);  // gleiche Verteilung
+    col_dsc[SYMBOLS_PER_ROW] = LV_GRID_TEMPLATE_LAST;
 
-static void update_ui() {
+    // Zeilenanzahl berechnen (abhängig von Symbolanzahl)
+    int total_symbols = sizeof(symbols) / sizeof(symbols[0]);
+    int rows = (total_symbols + SYMBOLS_PER_ROW - 1) / SYMBOLS_PER_ROW;
 
-    /*
-    if (on_usb_power) {
-        power_icon = LV_SYMBOL_USB;
-        lv_label_set_text(lbl_power_mode, "USB Power");
-        lv_label_set_text(lbl_battery_pct, "");
-    } else {
-        power_icon = battery_symbols[current_battery_symbol_idx];
-        if (battery_percentage > 100) {
-            battery_percentage = 100;
-        }
-        lv_label_set_text(lbl_power_mode, "Battery Power");
-        lv_label_set_text_fmt(lbl_battery_pct, "Charge Level: %d %%", battery_percentage);
+    static lv_coord_t row_dsc[100]; // ausreichend viele Zeilen
+    for (int i = 0; i < rows; ++i)
+        row_dsc[i] = LV_GRID_CONTENT;
+    row_dsc[rows] = LV_GRID_TEMPLATE_LAST;
+
+    // Grid aktivieren
+    lv_obj_set_layout(top_bar, LV_LAYOUT_GRID);
+    lv_obj_set_style_pad_column(top_bar, SPACING_X, 0);
+    lv_obj_set_style_pad_row(top_bar, SPACING_Y, 0);
+    lv_obj_set_grid_dsc_array(top_bar, col_dsc, row_dsc);
+
+    // Symbole als Labels hinzufügen
+    for (int i = 0; i < total_symbols; ++i)
+    {
+        lv_obj_t *label = lv_label_create(top_bar);
+        lv_label_set_text(label, symbols[i]);
+
+        // Optional: Stil setzen
+        lv_obj_set_style_text_align(label, LV_TEXT_ALIGN_CENTER, 0);
+        lv_obj_clear_flag(label, LV_OBJ_FLAG_SCROLLABLE);
+
+        // Im Grid positionieren
+        int col = i % SYMBOLS_PER_ROW;
+        int row = i / SYMBOLS_PER_ROW;
+        lv_obj_set_grid_cell(label, LV_GRID_ALIGN_CENTER, col, 1,
+                                       LV_GRID_ALIGN_CENTER, row, 1);
     }
-    lv_label_set_text_fmt(lbl_voltage, "%d mV", battery_voltage);
-
-
-    if (battery_percentage > 75 && battery_percentage <= 100) {
-        current_battery_symbol_idx = 4;
-    } else if (battery_percentage > 50 && battery_percentage <= 75) {
-        current_battery_symbol_idx = 3;
-    } else if (battery_percentage > 25 && battery_percentage <= 50) {
-        current_battery_symbol_idx = 2;
-    } else if (battery_percentage > 10 && battery_percentage <= 25) {
-        current_battery_symbol_idx = 1;
-    } else {
-        current_battery_symbol_idx = 0;
-    }
-
-    lv_label_set_text(lbl_power_icon, power_icon);
-
-
-
-    lv_label_set_text(lbl_wlan_icon, wlan_icon);
-    lv_label_set_text(lbl_bluetooth_icon, bluetooth_icon);
-    lv_label_set_text(lbl_gps_icon, gps_icon);
-    lv_label_set_text(lbl_audio_icon, audio_icon);
-    lv_label_set_text(lbl_video_icon, video_icon);
-    lv_label_set_text(lbl_list_icon, list_icon);
-    */
-
 }
 
-
-static void ui_update_task(void *pvParam) {
-    // setup the test ui
+static void ui_update_task(void *pvParam)
+{
     lvgl_port_lock(0);
     ui_init();
     lvgl_port_unlock();
 
-    while (1) {
-        // update the ui every 50 milliseconds
-        //vTaskDelay(pdMS_TO_TICKS(50));
-        if (lvgl_port_lock(0)) {
-            // update ui under lvgl semaphore lock
-            update_ui();
+    while (1)
+    {
+        if (lvgl_port_lock(0))
+        {
+            create_symbol_grid();
             lvgl_port_unlock();
         }
+        vTaskDelay(pdMS_TO_TICKS(50)); // Kurze Pause, damit der Task nicht blockiert
     }
-
-    // a freeRTOS task should never return ^^^
 }
-
-
+/*
 // increment lvgl timer
 static void lvgl_ticker_timer_cb(void *arg)
 {
-    /* Tell LVGL how many milliseconds have elapsed */
     lv_tick_inc(LVGL_TICK_PERIOD_MS);
 }
-
-static void ui_lvgl_demos_task(void *pvParam) {
-    // this is a workaround to get the lvgl demos working with esp-idf lvgl port
-    // lvgl demos run in lv timers and so lvgl port locks are not called resulting in errors
-    ESP_LOGI(TAG, "starting ui_lvgl_demos_task");
-
-    // first acquire port lock
-    lvgl_port_lock(0);
-
-    // stop lvgl port (lvgl tick timer)
-    if(lvgl_port_stop() == ESP_OK) {
-        ESP_LOGI(TAG, "lvgl_port_stop ok");
-    } else {
-        ESP_LOGE(TAG, "lvgl_port_stop error!");
-    }
-
-    // since lv ticker is stopped by lvgl_port_stop, we need to create a timer for tick
-    lv_timer_enable(true);
-    const esp_timer_create_args_t lvgl_tick_timer_args = {
-            .callback = &lvgl_ticker_timer_cb,
-            .name = "LVGL tick",
-    };
-    esp_timer_handle_t tick_timer;
-    esp_timer_create(&lvgl_tick_timer_args, &tick_timer);
-    esp_timer_start_periodic(tick_timer, LVGL_TICK_PERIOD_MS * 1000);
+*/
 
 
-    // infinite loop that handles the lv_timer_handler api calls
-    // similar logic to lvgl port
-    uint32_t task_delay_ms = 0;
-    for(;;) {
-        lvgl_port_lock(0);
-        if (lv_display_get_default()) {
-            task_delay_ms = lv_timer_handler();
-        } else {
-            task_delay_ms = 1;
-        }
-        if(task_delay_ms == LV_NO_TIMER_READY) {
-            task_delay_ms = LVGL_MAX_SLEEP_MS;
-        }
-        vTaskDelay(pdMS_TO_TICKS(task_delay_ms));
-        lvgl_port_unlock();
-    }
-}
-
-void app_main(void) {
+void app_main(void)
+{
     // LVGL display handle
     static lv_display_t *disp_handle;
-
-    // initialize the LCD
-    // don't turn on backlight yet - demo of gradual brightness increase is shown below
-    // otherwise you can set it to true to turn on the backlight at lcd init
-    //lcd_init(&disp_handle, false);
     lcd_init(&disp_handle, true);
-
-    /*
-    // Configure a periodic timer to update the battery voltage, brightness level etc
-    const esp_timer_create_args_t periodic_timer_args = {
-            .callback = &update_hw_info_timer_cb,
-            .name = "update_hw_info_timer"
-    };
-    */
-
-
-
-
-    //esp_timer_handle_t update_hw_info_timer_handle;
-    //ESP_ERROR_CHECK(esp_timer_create(&periodic_timer_args, &update_hw_info_timer_handle));
-
-    // update the hw info 250 milliseconds
-    //ESP_ERROR_CHECK(esp_timer_start_periodic(update_hw_info_timer_handle, 250 * 1000));
 
     // configure a FreeRTOS task, pinned to the second core (core 0 should be used for hw such as wifi, bt etc)
     xTaskCreatePinnedToCore(ui_update_task, "update_ui", 4096 * 2, NULL, 0, NULL, 1);
 
     // demonstrate the lcd brightness fade using aw9364 driver
-    lcd_set_brightness_pct_fade(100,3000);
+    lcd_set_brightness_pct_fade(100, 3000);
 
     // de-initialize lcd and other components
     // lvgl_port_remove_disp(disp_handle);
-
 }
